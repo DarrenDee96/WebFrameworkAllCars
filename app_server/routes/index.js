a272ab7276014cb35b9b7773a450f2cb9d85a4c5
DarrenDee96 <darren.dineen@students.ittralee.ie>
2022-10-22 12:42:37 +0100

    Commit

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -2,18 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 const ctrlHome = require('../controllers/home'); 
-const ctrlOthers = require('../controllers/others');
+const ctrlLogin = require('../controllers/login');
+const ctrlRegister = require('../controllers/register')
 
 
 /* Home page */
 router.get('/', ctrlHome.homePage);
-router.get('/location', ctrlLocations.loginPage);
-router.get('/location', ctrlLocations.registerPage);
+
 /* Login page */
-router.get('/about', ctrlOthers.about);
+router.get('/login', ctrlLogin.loginPage);
 module.exports = router;
 
 /* Register page */
-router.get('/about', ctrlOthers.about);
+router.get('/register', ctrlRegister.registerPage);
 module.exports = router;
 
